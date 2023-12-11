750d9ef86c04e7f5740e32b073751983334ca67a
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2023-12-11 20:14:37 +0545

    Added new utils functions

--- Contra-2D-Game/src/utils/utils.js
+++ Contra-2D-Game/src/utils/utils.js
@@ -1,8 +1,21 @@
-function detectCollision(rect1, rect2) {
+function detectBlockCollision(rect1, rect2) {
   return (
     rect1.xAxis < rect2.xAxis + rect2.width &&
     rect1.xAxis + rect1.width > rect2.xAxis &&
     rect1.yAxis < rect2.yAxis &&
     rect1.yAxis + rect1.height > rect2.yAxis
   );
+}
+function detectRectCollision(rect1, rect2) {
+  return (
+    rect1.xAxis < rect2.xAxis + rect2.width &&
+    rect1.xAxis + rect1.width > rect2.xAxis &&
+    rect1.yAxis < rect2.yAxis + rect2.height &&
+    rect1.yAxis + rect1.height > rect2.yAxis
+  );
+}
+
+function generateRandomNumber(min,max)
+{
+    return Math.random() * (max - min) + min;
 }
