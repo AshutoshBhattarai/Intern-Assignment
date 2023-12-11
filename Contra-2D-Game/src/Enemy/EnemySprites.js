68cd22dce90fa679bea82ef166bc78b77b5694d0
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2023-12-11 20:12:27 +0545

    updated sprite positions

--- Contra-2D-Game/src/Enemy/EnemySprites.js
+++ Contra-2D-Game/src/Enemy/EnemySprites.js
@@ -4,150 +4,150 @@ const runningEnemy = {
             x: 285,
             y: 0,
             height: 47,
-            width: 31
+            width: 36
         },
         {
-            x: 259,
+            x: 260,
             y: 0,
-            height: 47,
-            width: 31
+            height: 52,
+            width: 28
         },
         {
             x: 189,
             y: 0,
             height: 47,
-            width: 31
+            width: 45
         },
         {
-            x: 148,
+            x: 149,
             y: 0,
             height: 47,
-            width: 31
+            width: 36
         },
         {
-            x: 117,
+            x: 118,
             y: 0,
             height: 47,
-            width: 31
+            width: 34
         },
         {
-            x: 92,
+            x: 94,
             y: 0,
-            height: 47,
-            width: 31
+            height: 49,
+            width: 28
         },
         {
-            x: 58,
+            x: 60,
             y: 0,
             height: 47,
-            width: 31
+            width: 36
         },
         {
             x: 17,
             y: 0,
             height: 47,
-            width: 31
+            width: 45
         },
     ],
     runningRight: [
         {
-            x: 314,
+            x: 320,
             y: 0,
             height: 47,
-            width: 31
+            width: 32
         },
         {
             x: 354,
             y: 0,
-            height: 47,
-            width: 31
+            height: 52,
+            width: 26
         },
         {
             x: 381,
             y: 0,
             height: 47,
-            width: 31
+            width: 29
         },
         {
-            x: 410,
+            x: 409,
             y: 0,
             height: 47,
-            width: 31
+            width: 41
         },
         {
-            x: 449,
+            x: 452,
             y: 0,
             height: 47,
-            width: 31
+            width: 38
         },
         {
             x: 488,
             y: 0,
             height: 47,
-            width: 31
+            width: 32
         },
         {
             x: 520,
             y: 0,
-            height: 47,
-            width: 31
+            height: 50,
+            width: 25
         },
         {
             x: 544,
             y: 0,
             height: 47,
-            width: 31
+            width: 36
         },
     ]
 }
 
 const gunEnemy = {
     left : {
-        x : 45,
-        y : 257,
-        height : 51,
-        width : 33,
+        x : 52,
+        y : 255,
+        height : 52,
+        width : 42,
     },
     up : {
-        x : 255,
-        y : 257,
-        height : 51,
+        x : 263,
+        y : 248,
+        height : 60,
         width : 33,
     },
     down : {
-        x : 132,
-        y : 308,
+        x : 130,
+        y : 306,
         height : 51,
         width : 33,
     },
     right : {
-        x : 533,
+        x : 543,
         y : 257,
-        height : 51,
-        width : 33,
+        height : 50,
+        width : 43,
     },
     upLeft : {
-        x : 131,
-        y : 257,
-        height : 51,
-        width : 33,
+        x : 133,
+        y : 244,
+        height : 60,
+        width : 35,
     },
     upRight : {
-        x : 391,
-        y : 257,
-        height : 51,
+        x : 405,
+        y : 244,
+        height : 62,
         width : 33,
     },
     downLeft : {
-        x : 215,
-        y : 308,
-        height : 51,
-        width : 33,
+        x : 214,
+        y : 306,
+        height : 52,
+        width : 35,
     },
     downRight : {
-        x : 351,
-        y : 308,
-        height : 51,
+        x : 357,
+        y : 305,
+        height : 52,
         width : 33,
     }
 
