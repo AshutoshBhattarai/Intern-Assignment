2b0a49ea7aed12da8dbbcd6d8c582013552803f8
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2023-12-11 20:09:54 +0545

    Updated bullet class. Now used by both player and enemies

--- Contra-2D-Game/src/Characters/Bullet.js
+++ Contra-2D-Game/src/Characters/Bullet.js
@@ -1,51 +1,51 @@
 class Bullet {
-    constructor(xAxis, yAxis, direction) {
+    constructor(xAxis, yAxis, direction, from) {
         this.xAxis = xAxis;
         this.yAxis = yAxis;
         this.direction = direction;
+        this.from = from;
         this.width = 5;
+        this.bulletSpeed = this.from === ENEMY_SOLDIER ? ENEMY_BULLET_SPEED : BULLET_SPEED;
         this.height = 5;
 
     }
 
-    setXAxis(xAxis) {
-        this.xAxis = xAxis;
-    }
-    setYAxis(yAxis) {
-        this.yAxis = yAxis;
-    }
     draw(ctx) {
-        ctx.fillStyle = "red";
+        ctx.fillStyle = this.from === PLAYER_ID ? "red" : "yellow";
         ctx.fillRect(this.xAxis, this.yAxis, this.width, this.height);
     }
+
     update() {
         if (this.direction === DIRECTION_RIGHT) {
-            this.xAxis += BULLET_SPEED;
+            this.xAxis += this.bulletSpeed;
         }
         if (this.direction === DIRECTION_LEFT) {
-            this.xAxis -= BULLET_SPEED;
+            this.xAxis -= this.bulletSpeed;
         }
         if (this.direction === DIRECTION_UP) {
-            this.yAxis -= BULLET_SPEED;
+            this.yAxis -= this.bulletSpeed;
+        }
+        if (this.direction === DIRECTION_DOWN) {
+            this.yAxis += this.bulletSpeed;
         }
         if (this.direction === DIRECTION_DOWN_LEFT) {
-            this.yAxis += BULLET_SPEED;
-            this.xAxis -= BULLET_SPEED;
+            this.yAxis += this.bulletSpeed;
+            this.xAxis -= this.bulletSpeed;
         }
         if (this.direction === DIRECTION_DOWN_RIGHT) {
-            this.yAxis += BULLET_SPEED;
-            this.xAxis += BULLET_SPEED;
+            this.yAxis += this.bulletSpeed;
+            this.xAxis += this.bulletSpeed;
         }
         if (this.direction === DIRECTION_UP_LEFT) {
-            this.yAxis -= BULLET_SPEED;
-            this.xAxis -= BULLET_SPEED;
+            this.yAxis -= this.bulletSpeed;
+            this.xAxis -= this.bulletSpeed;
         }
         if (this.direction === DIRECTION_UP_RIGHT) {
-            this.yAxis -= BULLET_SPEED;
-            this.xAxis += BULLET_SPEED;
+            this.yAxis -= this.bulletSpeed;
+            this.xAxis += this.bulletSpeed;
         }
         if (this.isOutOfBounds()) {
-            this.removeBullet();
+            this.removeBullet(this.from);
         }
     }
 
@@ -54,10 +54,24 @@ class Bullet {
         const { width, height } = canvas;
         return xAxis > width || xAxis < 0 || yAxis > height - 40 || yAxis < 0;
     }
-    removeBullet() {
-        const bulletIndex = bullets.indexOf(this);
-        if (bulletIndex !== -1) {
-            bullets.splice(bulletIndex, 1);
+    removeBullet(from) {
+        if (from === PLAYER_ID) {
+            const bulletIndex = playerBullets.indexOf(this);
+            if (bulletIndex !== -1) {
+                playerBullets.splice(bulletIndex, 1);
+            }
         }
+        else {
+            const bulletIndex = enemyBullets.indexOf(this);
+            if (bulletIndex !== -1) {
+                enemyBullets.splice(bulletIndex, 1);
+            }
+        }
+    }
+    setXAxis(xAxis) {
+        this.xAxis = xAxis;
+    }
+    setYAxis(yAxis) {
+        this.yAxis = yAxis;
     }
 }
