d59ff63a8b472ccdf35a5b18384af0f3aebd477c
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2023-12-11 21:53:54 +0545

    Added new sprite images with coordinates

---
+++ Contra-2D-Game/src/Characters/ExtraSprites.js
@@ -0,0 +1,82 @@
+const healthDisplaySprite = {
+    x: 282,
+    y: 264,
+    height: 24,
+    width: 17,
+
+}
+const extraHealth = {
+    x: 290,
+    y: 143,
+    height: 16,
+    width: 25
+}
+const playerBulletSprite = {
+    x: 260,
+    y: 501,
+    height: 3,
+    width: 3
+}
+const enemyBulletSprite = {
+    x: 256,
+    y: 502,
+    height: 3,
+    width: 3
+}
+const explosionBig = {
+    x: 156,
+    y: 504,
+    height: 32,
+    width: 32
+}
+const explosionMedium = {
+    x: 189,
+    y: 506,
+    height: 27,
+    width: 23
+}
+const explosionSmall = {
+    x: 217,
+    y: 518,
+    height: 18,
+    width: 15
+}
+
+const doorPowerups = [
+    {
+        x: 35,
+        y: 0,
+        height: 32,
+        width: 32,
+    },
+    {
+        x: 66,
+        y: 0,
+        height: 32,
+        width: 32,
+    },
+    {
+        x: 101,
+        y: 0,
+        height: 32,
+        width: 32,
+    },
+    {
+        x: 134,
+        y: 0,
+        height: 32,
+        width: 32,
+    },
+    {
+        x: 169,
+        y: 0,
+        height: 32,
+        width: 32,
+    },
+    {
+        x: 237,
+        y: 0,
+        height: 32,
+        width: 32,
+    },
+]
