e7abe214b2bfe8e7328b4769aa78193745cf1097
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2023-12-14 20:03:51 +0545

    Added audio paths

---
+++ Contra-2D-Game/src/Map/Audio.js
@@ -0,0 +1,11 @@
+const gameAudios = {
+    title: './assets/sound/title.mp3',
+    menuSelect: './assets/sound/menu-selection.mp3',
+    menuHover: './assets/sound/menu-button.mp3',
+    gameOver: './assets/sound/game-over.mp3.mp3',
+    gameMusic: './assets/sound/Contra-Jungle.mp3',
+    gameOverMusic: './assets/sound/game-over.mp3',
+    enemyHit: './assets/sound/enemy-hit.mp3',
+    playerHit: './assets/sound/dead.mp3',
+    playerGun : "./assets/sound/enemy-gun.mp3"
+}
