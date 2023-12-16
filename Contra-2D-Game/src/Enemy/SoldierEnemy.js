4add33bedc14c48bab4eaae7801fceacc99a8710
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2023-12-16 10:46:08 +0545

    Refactored getDirection

--- Contra-2D-Game/src/Enemy/SoldierEnemy.js
+++ Contra-2D-Game/src/Enemy/SoldierEnemy.js
@@ -20,4 +20,90 @@ class SoldierEnemy extends Enemy {
             this.useGravity()
         }
     }
+    shoot(player) {
+        const direction = this.getEnemyShootingDirection(player);
+        // Get the current time
+        const currentTime = Date.now();
+        // Check if enough time has passed since the enemy last shot a bullet
+        const canShoot = currentTime - enemyLastBullet > BULLET_COOLDOWN;
+        // Set the origin of the bullet to the enemy soldier
+        const from = ENEMY_SOLDIER;
+        // If enough time has passed, determine the position and direction of the bullet based on the given direction
+        if (canShoot) {
+            let bulletX, bulletY, bulletDirection;
+            if (direction === DIRECTION_LEFT) {
+                bulletX = this.xAxis;
+                bulletY = this.yAxis + this.height / 3;
+                bulletDirection = DIRECTION_LEFT;
+            } else if (direction === DIRECTION_RIGHT) {
+                bulletX = this.xAxis + PLAYER_WIDTH;
+                bulletY = this.yAxis + this.height / 3;
+                bulletDirection = DIRECTION_RIGHT;
+            } else if (direction === DIRECTION_UP) {
+                bulletX = this.xAxis + this.width / 2;
+                bulletY = this.yAxis;
+                bulletDirection = DIRECTION_UP;
+            } else if (direction === DIRECTION_UP_LEFT) {
+                bulletX = this.xAxis;
+                bulletY = this.yAxis;
+                bulletDirection = DIRECTION_UP_LEFT;
+            } else if (direction === DIRECTION_UP_RIGHT) {
+                bulletX = this.xAxis + this.width;
+                bulletY = this.yAxis;
+                bulletDirection = DIRECTION_UP_RIGHT;
+            } else if (direction === DIRECTION_DOWN_LEFT) {
+                bulletX = this.xAxis;
+                bulletY = this.yAxis + this.height / 2;
+                bulletDirection = DIRECTION_DOWN_LEFT;
+            } else if (direction === DIRECTION_DOWN_RIGHT) {
+                bulletX = this.xAxis + this.width;
+                bulletY = this.yAxis + this.height / 2;
+                bulletDirection = DIRECTION_DOWN_RIGHT;
+            }
+            if (bulletDirection) {
+                shootBullet(bulletX, bulletY, bulletDirection, from);
+                enemyLastBullet = currentTime;
+            }
+        }
+    }
+    getEnemyShootingDirection(player) {
+        const { xAxis, yAxis } = player;
+        const { xAxis: enemyXAxis, yAxis: enemyYAxis } = this;
+        const isPlayerToLeft = xAxis < enemyXAxis - (PLAYER_WIDTH + 20);
+        const isPlayerToRight = xAxis > enemyXAxis + (PLAYER_WIDTH + 20);
+        const isPlayerAbove = yAxis < enemyYAxis - PLAYER_HEIGHT;
+        const isPlayerBelow = yAxis > enemyYAxis + PLAYER_HEIGHT;
+        if (isPlayerToLeft && isPlayerAbove) {
+            this.actions = gunEnemy.upLeft;
+            return DIRECTION_UP_LEFT;
+        }
+        else if (isPlayerToRight && isPlayerAbove) {
+            this.actions = gunEnemy.upRight;
+            return DIRECTION_UP_RIGHT;
+        }
+        else if (isPlayerToRight && isPlayerBelow) {
+            this.actions = gunEnemy.downRight;
+            return DIRECTION_DOWN_RIGHT;
+        }
+        else if (isPlayerToLeft && isPlayerBelow) {
+            this.actions = gunEnemy.downLeft;
+            return DIRECTION_DOWN_LEFT;
+        }
+        else if (isPlayerToLeft) {
+            this.actions = gunEnemy.left;
+            return DIRECTION_LEFT;
+        }
+        else if (isPlayerAbove) {
+            this.actions = gunEnemy.up;
+            return DIRECTION_UP;
+        }
+        else if (isPlayerToRight) {
+            this.actions = gunEnemy.right;
+            return DIRECTION_RIGHT;
+        }
+        else if (isPlayerBelow) {
+            this.actions = gunEnemy.down;
+            return DIRECTION_DOWN;
+        }
+    }
 }
