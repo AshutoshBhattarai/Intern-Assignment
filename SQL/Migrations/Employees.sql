c98c217dacdf64a1891bab517bfd89b21aeb8280
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2023-12-26 20:23:23 +0545

    updated sql

--- SQL/Migrations/Employees.sql
+++ SQL/Migrations/Employees.sql
@@ -1,8 +1,8 @@
 -- Active: 1703450346417@@localhost@5432@ashutosh@public
 CREATE Table employees(
     employeeID int PRIMARY KEY,
-    firstName VARCHAR,
     lastName VARCHAR,
+    firstName VARCHAR,
     birthDate DATE,
     photo VARCHAR,
     notes VARCHAR
