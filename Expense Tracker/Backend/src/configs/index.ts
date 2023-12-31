505799c38e316449281eabed13716e35fd62c98e
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2023-12-31 18:41:31 +0545

    Add db connection variables

--- Expense Tracker/Backend/src/configs/index.ts
+++ Expense Tracker/Backend/src/configs/index.ts
@@ -3,5 +3,12 @@ dotenv.config();
 
 const config = {
   serverPort: process.env.PORT || 5005,
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT!,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+  },
 };
 export default config;
