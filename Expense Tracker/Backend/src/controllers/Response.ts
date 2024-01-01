27cd071edd12bb476ffecb66d89d08cac06b658f
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-01 21:31:44 +0545

    Added unauthorized Response

--- Expense Tracker/Backend/src/controllers/Response.ts
+++ Expense Tracker/Backend/src/controllers/Response.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 type ResponseData = {
-  message: string;
+  message?: string;
   result?: Object | Object[] | null;
 };
 export const successResponse = (res: Response, data: ResponseData) => {
@@ -20,3 +20,12 @@ export const errorResponse = (res: Response, data: ResponseData) => {
     data: data.result,
   });
 };
+
+export const unauthorizedResponse = (res: Response, data: ResponseData) => {
+  res.status(403).json({
+    success: false,
+    errorCode: 403,
+    message: "Unauthorized",
+    data: data.result,
+  });
+}
