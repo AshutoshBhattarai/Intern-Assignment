90c14b8e2c40c1acc8d589f3c3c8652e3681fdc0
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-01 16:18:49 +0545

    Updated User

--- Expense Tracker/Backend/src/models/User.ts
+++ Expense Tracker/Backend/src/models/User.ts
@@ -1,9 +1,8 @@
-import { randomUUID } from "crypto";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity("users")
 export class User {
   @PrimaryGeneratedColumn("uuid")
-  id!: string;
+  id: string | undefined;
   @Column()
   name: string;
   @Column({ unique: true })
@@ -14,16 +13,11 @@ export class User {
   createdAt: Date;
   @Column()
   updatedAt: Date;
-  @Column({ name: "refresh_token", nullable: true })
-  refreshToken?: string;
-  @Column({ name: "reset_code" })
-  resetCode: string;
   constructor(name: string, email: string, password: string) {
     this.name = name;
     this.email = email;
     this.password = password;
     this.createdAt = new Date();
     this.updatedAt = new Date();
-    this.resetCode = randomUUID();
   }
 }
