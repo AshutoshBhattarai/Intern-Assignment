fc49776f0300beddd8c3c97c77e3f735f1835856
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-01 17:26:11 +0545

    added authentication routes

--- Expense Tracker/Backend/src/routes/index.ts
+++ Expense Tracker/Backend/src/routes/index.ts
@@ -1,14 +1,22 @@
 import { Router } from "express";
 import users from "./User";
+import auth from "./Auth";
 const router = Router();
 
-// Initial route
+/* -------------------------------------------------------------------------- */
+/*                                   Routes                                   */
+/* -------------------------------------------------------------------------- */
+/* --------------------------- Initial Home Route --------------------------- */
 router.get("/", (req, res) => {
   res.json({
     message: "Hello World from the Expense Tracker API!!",
   });
 });
 
+/* ------------------------------- Sub Routes ------------------------------- */
+// User routes
 router.use("/users", users);
+// Authentication routes
+router.use(auth);
 // Export the router
 export default router;
