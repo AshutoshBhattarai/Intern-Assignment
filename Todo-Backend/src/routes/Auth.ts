a1335d309b8c229e0ecbf48e5a2e30025dfa0d57
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-02 20:57:56 +0545

    Added Data Validation

--- Todo-Backend/src/routes/Auth.ts
+++ Todo-Backend/src/routes/Auth.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { login, signup, refresh } from "../controller/AuthController";
+import { validateReqBody } from "../middleware/validator";
+import { loginSchema, signupSchema } from "../schema/userSchema";
 const router = Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/signup", validateReqBody(signupSchema), signup);
+router.post("/login", validateReqBody(loginSchema), login);
 router.post("/refresh", refresh);
 export default router;
