c9a484ee9a7b35802afc2897dc988f8dbf15f1be
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-03 17:28:24 +0545

    Update: Changes

--- Todo-Backend/src/utils/config.ts
+++ Todo-Backend/src/utils/config.ts
@@ -7,6 +7,14 @@ const config = {
     accessTokenSecret: process.env.JWT_ACCESS_SECRET_KEY,
     refreshTokenSecret: process.env.JWT_REFRESH_SECRET_KEY,
   },
+  database: {
+    client: "pg",
+    host: process.env.DATABASE_HOST!,
+    port: Number(process.env.DATABASE_PORT!),
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASSWORD!,
+    database: process.env.DATABASE_NAME!,
+  },
 };
 
 export default config;
