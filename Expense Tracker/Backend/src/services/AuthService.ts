7b73000b253357168a8b039be3e772ca60d4294b
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-03 20:29:18 +0545

    Changed Error Types

--- Expense Tracker/Backend/src/services/AuthService.ts
+++ Expense Tracker/Backend/src/services/AuthService.ts
@@ -1,4 +1,4 @@
-import { User } from "../models/User";
+import User from "../models/User";
 import * as userRepo from "../repositories/UserRepo";
 import argon2 from "argon2";
 import jwt from "jsonwebtoken";
@@ -7,15 +7,20 @@ import {
   JWT_ACCESS_TOKEN_EXPIRY,
   JWT_REFRESH_TOKEN_EXPIRY,
 } from "../constants";
+import NotFoundError from "../errors/NotFound";
+import ForbiddenError from "../errors/Forbidden";
 
 export const register = async (user: User) => {
   try {
     const userExists = await userRepo.getUserByEmail(user.email);
     if (userExists) {
-      throw new Error(`User with email ${user.email} already exists`);
+      throw new NotFoundError(`User with email ${user.email} already exists`);
     }
     const hashedPassword = await argon2.hash(user.password);
-    const newUser = new User(user.username, user.email, hashedPassword);
+    const newUser = new User();
+    newUser.username = user.username;
+    newUser.email = user.email;
+    newUser.password = hashedPassword;
     await userRepo.addUser(newUser);
     return newUser;
   } catch (error) {
@@ -27,7 +32,7 @@ export const login = async (user: User) => {
   try {
     const foundUser = await userRepo.getUserByEmail(user.email);
     if (!foundUser) {
-      throw new Error(`User with email ${user.email} not found`);
+      throw new NotFoundError(`User with email ${user.email} not found`);
     }
     const isPasswordValid = await argon2.verify(
       foundUser.password,
@@ -35,7 +40,7 @@ export const login = async (user: User) => {
     );
 
     if (!isPasswordValid) {
-      throw new Error("Invalid password");
+      throw new ForbiddenError("Invalid password");
     }
 
     const accessToken = createAccessToken(foundUser.id);
@@ -62,9 +67,9 @@ export const logout = async (user: User) => {
 export const refresh = async (id: string, refreshToken: string) => {
   try {
     const user = await userRepo.getUserById(id);
-    if (!user) throw new Error("User not found");
+    if (!user) throw new NotFoundError("User not found");
     if (user.refreshToken !== refreshToken)
-      throw new Error("Invalid refresh token");
+      throw new ForbiddenError("Invalid refresh token");
 
     const accessToken = createAccessToken(user.id);
     const newRefreshToken = createRefreshToken(user.id);
