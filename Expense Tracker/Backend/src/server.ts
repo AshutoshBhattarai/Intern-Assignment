02b00e2d8666054987cd08a5b641e7a08667da69
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-03 08:40:52 +0545

    Update: Added Middlewares

--- Expense Tracker/Backend/src/server.ts
+++ Expense Tracker/Backend/src/server.ts
@@ -1,17 +1,29 @@
+/* --------------------------------- Imports -------------------------------- */
 import express, { Express } from "express";
 import "reflect-metadata";
 import config from "./configs";
 import routes from "./routes";
 import databaseConnection from "./database/connection";
+import logHandler from "./middlewares/LogHandler";
+import errorHandler from "./middlewares/ErrorHandler";
+
+/* -------------------------- Server initialization ------------------------- */
 const app: Express = express();
 const PORT = config.serverPort;
+/* --------------------------- Database connection -------------------------- */
 databaseConnection();
+/* ------------------------------- Middlewares ------------------------------ */
 // Middleware to parse request body
 app.use(express.json());
 // Middleware to parse url encoded request body
 app.use(express.urlencoded({ extended: false }));
+// Middleware to handle logging
+app.use(logHandler);
 // Middleware to handle routes
 app.use(routes);
+// Middleware to handle errors
+app.use(errorHandler);
+/* --------------------------- Running the server --------------------------- */
 // Server is running on port 5005
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
