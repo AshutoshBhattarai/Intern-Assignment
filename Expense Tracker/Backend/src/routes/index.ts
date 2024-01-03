c478974167ace1383349d7a02680141ecccc7697
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-03 21:18:42 +0545

    Added New Routes

--- Expense Tracker/Backend/src/routes/index.ts
+++ Expense Tracker/Backend/src/routes/index.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import users from "./User";
 import auth from "./Auth";
+import budget from "./Budget";
+import category from "./Category";
+import expenses from "./Expense";
+import images from "./Images";
+import income from "./Income";
+import users from "./User";
 import { jwtAuth } from "../middlewares/JwtAuth";
 const router = Router();
 
@@ -15,9 +20,19 @@ router.get("/", (req, res) => {
 });
 
 /* ------------------------------- Sub Routes ------------------------------- */
-// User routes
-router.use("/users", jwtAuth, users);
 // Authentication routes
 router.use(auth);
+// User routes
+router.use("/users", jwtAuth, users);
+// Expense routes
+router.use("/expenses", jwtAuth, expenses);
+// Category routes
+router.use("/categories", jwtAuth, category);
+// Budget routes
+router.use("/budgets", jwtAuth, budget);
+// Income routes
+router.use("/incomes", jwtAuth, income);
+// Image routes
+router.use("/images", images);
 // Export the router
 export default router;
