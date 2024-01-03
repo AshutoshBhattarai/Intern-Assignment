670540e28c20cd42fa4fe879455647bf15b51628
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-03 20:29:08 +0545

    Removed Constructor Initialization

--- Expense Tracker/Backend/src/models/User.ts
+++ Expense Tracker/Backend/src/models/User.ts
@@ -1,29 +1,21 @@
 import { randomUUID } from "crypto";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity("users")
-export class User {
+export default class User {
   @PrimaryGeneratedColumn("uuid")
-  id!: string;
+  id: string;
   @Column()
   username: string;
   @Column({ unique: true })
   email: string;
   @Column()
   password: string;
-  @Column()
+  @Column({default : new Date()})
   createdAt: Date;
-  @Column()
+  @Column({default : new Date()})
   updatedAt: Date;
   @Column({ nullable: true, name: "refresh_token" })
   refreshToken?: string;
-  @Column({ name: "reset_code" })
+  @Column({ name: "reset_code",default:randomUUID() })
   resetCode: string;
-  constructor(username: string, email: string, password: string) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-    this.createdAt = new Date();
-    this.updatedAt = new Date();
-    this.resetCode = randomUUID();
-  }
 }
