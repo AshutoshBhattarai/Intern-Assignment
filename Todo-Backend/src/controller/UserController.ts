cbab332b63ff3938b22df90437d5710d0587d79f
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-03 08:44:23 +0545

    Update: Handles Errors and Changes

--- Todo-Backend/src/controller/UserController.ts
+++ Todo-Backend/src/controller/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import * as userService from "../service/UserService";
+import { NotFoundError } from "../errors/NotFoundError";
 export const getUsers = (req: Request, res: Response) => {
   const users = userService.getAllUsers();
   res.json({
@@ -11,18 +12,12 @@ export const getUserById = (req: Request, res: Response) => {
   const id = parseInt(req.params.id);
   const user = userService.getUserById(id);
   if (user) return res.json({ message: user });
-  else
-    return res.json({
-      message: "User not found",
-    });
+  else throw new NotFoundError("User not found");
 };
 
 export const getUserByEmail = (req: Request, res: Response) => {
   const email = req.params.email;
   const user = userService.getUserByEmail(email);
   if (user) return res.json({ message: user });
-  else
-    return res.json({
-      message: "User not found",
-    });
+  else throw new NotFoundError("User not found");
 };
--- Todo-Backend/src/service/TodoService.ts
+++ Todo-Backend/src/service/TodoService.ts
@@ -1,3 +1,4 @@
+import { NotFoundError } from "../errors/NotFoundError";
 import * as todoRepo from "../Repositories/TodoRepo";
 import { Todo } from "../model/TodoModel";
 
@@ -6,21 +7,29 @@ export const getAllTodos = () => {
 };
 
 export const getUserTodos = (id: number) => {
-  return todoRepo.getUserTodos(id);
-}
+  const todos = todoRepo.getUserTodos(id);
+  if (!todos) throw new NotFoundError("Todos not found");
+  return todos;
+};
 
 export const addTodo = (todo: Todo) => {
   return todoRepo.addTodo(todo);
 };
 
-export const getTodoById = (id: number,userid : number) => {
-  return todoRepo.getTodoById(id,userid);
+export const getTodoById = (id: number, userid: number) => {
+  const todos = todoRepo.getTodoById(id, userid);
+  if (!todos) throw new NotFoundError("Todo not found");
+  return todos;
 };
 
-export const updateTodo = (id:number,todo: Todo) => {
-  return todoRepo.updateTodo(id,todo);
+export const updateTodo = (id: number, todo: Todo) => {
+  const foundTodo = todoRepo.getTodoById(id, todo.userid);
+  if (!foundTodo) throw new NotFoundError("Todo not found");
+  return todoRepo.updateTodo(id, todo);
 };
 
-export const deleteTodo = (id: number) => {
+export const deleteTodo = (id: number, userid: number) => {
+  const todo = todoRepo.getTodoById(id, userid);
+  if (!todo) throw new NotFoundError("Todo not found");
   return todoRepo.deleteTodo(id);
 };
