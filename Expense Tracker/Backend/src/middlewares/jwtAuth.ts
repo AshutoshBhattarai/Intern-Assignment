2e1321a157b49680f7b8497373b3e585d7b7c7f7
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-04 08:04:49 +0545

    Updated error response

--- Expense Tracker/Backend/src/middlewares/jwtAuth.ts
+++ Expense Tracker/Backend/src/middlewares/jwtAuth.ts
@@ -1,26 +1,20 @@
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
-import { errorResponse, unauthorizedResponse } from "../controllers/Response";
-import config from "../configs";
+import config from "../configs/Index";
+import ForbiddenError from "../errors/Forbidden";
 
 export const jwtAuth = (req: Request, res: Response, next: NextFunction) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
-    if (!token)
-      unauthorizedResponse(res, {
-        result: "No token provided",
-      });
+    if (!token) throw new ForbiddenError("No token provided");
     const tokenUser = jwt.verify(token!, config.jwt.accessSecret);
-    if (!tokenUser)
-      unauthorizedResponse(res, {
-        result: "Invalid token",
-      });
-    (req as any).user = (tokenUser as any).id;
-    next();
-  } catch (error: any) {
-    errorResponse(res, {
-      message: "Something went wrong",
-      result: error.message,
-    });
+    if (!tokenUser) throw new ForbiddenError("Invalid access token");
+    else {
+      console.log(tokenUser);
+      (req as any).user = (tokenUser as any).userid;
+      next();
+    }
+  } catch (error) {
+    throw error;
   }
 };
