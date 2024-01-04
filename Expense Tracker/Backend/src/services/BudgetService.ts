f489527312a1ec76f5ee9e225e3fc979df40802e
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-04 22:31:45 +0545

    Added Budget Service

---
+++ Expense Tracker/Backend/src/services/BudgetService.ts
@@ -0,0 +1,30 @@
+import NotFoundError from "../errors/NotFound";
+import Budget from "../models/Budget";
+import User from "../models/User";
+import * as budgetRepo from "../repositories/BudgetRepo";
+import * as categoryRepo from "../repositories/CategoryRepo";
+import * as userRepo from "../repositories/UserRepo";
+
+export const createBudget = async (user: User, budget: Budget) => {
+  if (!(await userRepo.getUserById(user.id)))
+    throw new NotFoundError("User not found");
+  if (!await categoryRepo.getCategory(budget.category as any))
+    throw new NotFoundError("Category not found");
+  budget.user = user;
+  const newBudget = await budgetRepo.createBudget(budget);
+  return newBudget;
+};
+
+export const getAllBudgets = async (user: User) => {
+  console.log(await budgetRepo.getTotalBudget(user));
+  const budgets = await budgetRepo.getBudget(user);
+  return budgets;
+};
+
+export const getBudgetById = async (user: User, id: string) => {};
+
+export const getBudgetByTime = async (startTime: Date, endTime: Date) => {};
+
+export const updateBudget = async (id: string, budget: Budget) => {};
+
+export const deleteBudget = async (id: string) => {};
