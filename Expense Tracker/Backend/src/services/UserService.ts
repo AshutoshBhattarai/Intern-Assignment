a39f90d36c9d3849bdca3f9441a5938aba2f6938
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-04 08:06:50 +0545

    Updated Errors and added other logics

--- Expense Tracker/Backend/src/services/UserService.ts
+++ Expense Tracker/Backend/src/services/UserService.ts
@@ -1,25 +1,41 @@
+import NotFoundError from "../errors/NotFound";
 import User from "../models/User";
 import * as userRepo from "../repositories/UserRepo";
 
 export const getAllUsers = async (): Promise<User[]> => {
-  return await userRepo.getAllUsers();
+  const users = await userRepo.getAllUsers();
+  return users.map((user) => filterUser(user));
 };
 
-export const getUserById = async (id: string): Promise<User | null> => {
-  return await userRepo.getUserById(id);
+export const getUserById = async (id: string): Promise<User> => {
+  const user = await userRepo.getUserById(id);
+  if (!user) throw new NotFoundError(`User with id : ${id} not found`);
+  return filterUser(user);
 };
 
-export const getUserByEmail = async (email: string): Promise<User | null> => {
-  return await userRepo.getUserByEmail(email);
+export const getUserByEmail = async (email: string): Promise<User> => {
+  const user = await userRepo.getUserByEmail(email);
+  if (!user) throw new NotFoundError(`User with email : ${email} not found`);
+  return filterUser(user);
 };
 
 export const deleteUser = async (id: string) => {
   const user = await userRepo.getUserById(id);
-  if (!user) throw new Error("User not found");
+  if (!user) throw new NotFoundError("User not found");
   await userRepo.deleteUser(user);
 };
 
 export const updateUser = async (id: string) => {
   const user = await userRepo.getUserById(id);
-  if (!user) throw new Error("User not found");
+  if (!user) throw new NotFoundError("User not found");
+};
+
+const filterUser = (user: User) => {
+  const userInfo = new User();
+  userInfo.id = user.id;
+  userInfo.email = user.email;
+  userInfo.username = user.username;
+  userInfo.createdAt = user.createdAt;
+  userInfo.updatedAt = user.updatedAt;
+  return userInfo;
 };
