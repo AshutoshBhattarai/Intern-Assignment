8d0fcafea01474554bb9eae12770d1d17137f555
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-04 22:31:30 +0545

    Update: Added User Validation

--- Expense Tracker/Backend/src/services/CategoryService.ts
+++ Expense Tracker/Backend/src/services/CategoryService.ts
@@ -3,11 +3,14 @@ import NotFoundError from "../errors/NotFound";
 import Category from "../models/Category";
 import User from "../models/User";
 import * as categoryRepo from "../repositories/CategoryRepo";
+import * as userRepo from "../repositories/UserRepo";
 export const createCategory = async (category: Category) => {
+  const user = await userRepo.getUserById(category.user.id);
+  if (!user) throw new NotFoundError("User not found");
   return categoryRepo.createCategory(category);
 };
 
-export const getAllCategories = async (user : User) => {
+export const getAllCategories = async (user: User) => {
   return await categoryRepo.getAllCategories(user);
 };
 
@@ -15,19 +18,18 @@ export const getCategory = async (id: string) => {
   return await categoryRepo.getCategory(id);
 };
 
-export const updateCategory = async (userid: User, category: Category) => {
-
+export const updateCategory = async (user: User, category: Category) => {
   const exists = await categoryRepo.getCategory(category.id);
   if (!exists) throw new NotFoundError("Category does not exist");
-  if (userid != exists.user)
+  if (user.id != (exists.user as any))
     throw new ForbiddenError("You are not authorized to update this category");
   return categoryRepo.updateCategory(category.id, category);
 };
 
-export const deleteCategory = async (userid: User, categoryId: string) => {
+export const deleteCategory = async (user: User, categoryId: string) => {
   const exists = await categoryRepo.getCategory(categoryId);
   if (!exists) throw new NotFoundError("Category does not exist");
-  if (userid != exists.user)
+  if (user.id != (exists.user as any))
     throw new ForbiddenError("You are not authorized to delete this category");
   return categoryRepo.deleteCategory(categoryId);
 };
