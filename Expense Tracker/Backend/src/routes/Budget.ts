e92703bb35fad1aa7f5fbc42124ecd7c943f0fb4
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-04 22:26:30 +0545

    Added routes

--- Expense Tracker/Backend/src/routes/Budget.ts
+++ Expense Tracker/Backend/src/routes/Budget.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 const router = Router();
+import { createBudget,getAllBudgets,getBudgetById } from "../controllers/BudgetController";
 
-router.route("/").get().post();
-router.route("/:id").get().patch().delete();
+router.route("/").get(getAllBudgets).post(createBudget);
+router.route("/:id").get(getBudgetById).patch().delete();
 
 export default router;
