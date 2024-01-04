f9955d48e18fd629144f4b44a8ca25a9cc99466d
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-04 08:05:10 +0545

    fix: added missing next

--- Expense Tracker/Backend/src/middlewares/LogHandler.ts
+++ Expense Tracker/Backend/src/middlewares/LogHandler.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import logger from "../utils/logger";
 
-const logHandler = (req: Request, _res: Response, _next: NextFunction) => {
+const logHandler = (req: Request, _res: Response, next: NextFunction) => {
   logger.info(`${req.method}: ${req.path}`);
+  next();
 };
 export default logHandler;
