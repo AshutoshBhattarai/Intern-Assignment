c7d1e27e885e2d63d6f693069c3fc88097c997cb
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-05 15:54:20 +0545

    Update Providing User to other routes

--- Expense Tracker/Backend/src/middlewares/jwtAuth.ts
+++ Expense Tracker/Backend/src/middlewares/jwtAuth.ts
@@ -7,26 +7,30 @@ import jwt, {
 import config from "../configs/Index";
 import ForbiddenError from "../errors/Forbidden";
 import UnauthorizedError from "../errors/Unauthorized";
+import { getUserById } from "../services/UserService";
 
-export const jwtAuth = (req: Request, res: Response, next: NextFunction) => {
+export const jwtAuth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) throw new ForbiddenError("No token provided");
     const tokenUser = jwt.verify(token!, config.jwt.accessSecret);
     if (!tokenUser) throw new ForbiddenError("Invalid access token");
     else {
-      (req as any).user = (tokenUser as any).userid;
+      const userid = (tokenUser as any).userid;
+      res.locals.user = await getUserById(userid);
       next();
     }
   } catch (error) {
     if (error instanceof TokenExpiredError) {
-      throw new UnauthorizedError("Access Token has expired!");
-    }
-    if (error instanceof NotBeforeError) {
-      throw new UnauthorizedError("JWT is not active");
-    }
-    if (error instanceof JsonWebTokenError) {
-      throw new UnauthorizedError("JWT is malformed");
+      next(new UnauthorizedError("Access Token has expired!"));
+    } else if (error instanceof NotBeforeError) {
+      next(new UnauthorizedError("JWT is not active"));
+    } else if (error instanceof JsonWebTokenError) {
+      next(new UnauthorizedError("JWT is malformed"));
     }
     next(error);
   }
