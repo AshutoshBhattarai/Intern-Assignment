a9ed1ae3aeab6e8c94e9b6a612a84804168b2e27
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-05 15:53:48 +0545

    Update income retrival

--- Expense Tracker/Backend/src/repositories/IncomeRepo.ts
+++ Expense Tracker/Backend/src/repositories/IncomeRepo.ts
@@ -1,25 +1,24 @@
-import { Repository } from "typeorm"
-import Income from "../models/Income"
-import database from "../database/config"
-import User from "../models/User"
+import { Repository } from "typeorm";
+import Income from "../models/Income";
+import database from "../database/config";
+import User from "../models/User";
 
-const repo : Repository<Income> = database.getRepository("income")
-export const getIncome = async (user : User) => {
-    return await repo.find({
-        where : {user},
-    })
-}
+const repo: Repository<Income> = database.getRepository("income");
+export const getIncome = async (user: User) => {
+  return await repo.find({
+    where: { user: { id: user.id } },
+    relations: { user: true },
+  });
+};
 
-export const createIncome = async (income : Income) => {
-    return await repo.save(income)
-}
+export const createIncome = async (income: Income) => {
+  return await repo.save(income);
+};
 
-export const updateIncome = async (income : Income) => {
-    return await repo.update({id : income.id}, income)    
-}
+export const updateIncome = async (income: Income) => {
+  return await repo.update({ id: income.id }, income);
+};
 
-export const deleteIncome = async (income : Income) => {
-    return await repo.delete({id : income.id})
-}
-
-
+export const deleteIncome = async (income: Income) => {
+  return await repo.delete({ id: income.id });
+};
