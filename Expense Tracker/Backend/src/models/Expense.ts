3c0ca258336be2259fc1be8a12602eae8d6d5d38
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-06 11:36:14 +0545

    Update Model

--- Expense Tracker/Backend/src/models/Expense.ts
+++ Expense Tracker/Backend/src/models/Expense.ts
@@ -2,6 +2,7 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
   RelationId,
@@ -20,13 +21,15 @@ export default class Expense {
   date: Date;
   @Column()
   description: string;
-  @Column()
+  @Column({ default: "" })
   image: string;
   @ManyToOne(() => User, { onDelete: "CASCADE" })
   @RelationId((expense: Expense) => expense.user)
+  @JoinColumn({name: "user_id"})
   user: User;
   @ManyToOne(() => Category, { onDelete: "SET NULL" })
   @RelationId((expense: Expense) => expense.category)
+  @JoinColumn({name: "category_id"})
   category: Category;
   @CreateDateColumn({ default: new Date(), name: "created_at", update: false })
   createdAt: Date;
