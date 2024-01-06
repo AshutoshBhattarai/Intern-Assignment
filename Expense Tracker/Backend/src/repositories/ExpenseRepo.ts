94f70faf7d9af40dce10f8db88188ce0c2152988
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-06 20:47:44 +0545

    Added Filtering Based on query parameter

--- Expense Tracker/Backend/src/repositories/ExpenseRepo.ts
+++ Expense Tracker/Backend/src/repositories/ExpenseRepo.ts
@@ -1,7 +1,10 @@
-import { Repository } from "typeorm";
-import Expense from "../models/Expense";
+import { Between, FindOptionsWhere, ILike, Raw, Repository } from "typeorm";
 import database from "../database/config";
+import Expense from "../models/Expense";
 import User from "../models/User";
+import { ExpenseQuery } from "../types/QueryType";
+import Category from "../models/Category";
+import date from "date-fns";
 
 const repo: Repository<Expense> = database.getRepository("expenses");
 
@@ -27,10 +30,39 @@ export const getExpensesById = async (id: string) => {
   return await repo.findOneBy({ id });
 };
 
-export const getExpenseByDate = (
+// Todo Fix the code
+//! Date range is not working
+export const getTotalExpenseByDate = async (
   startDate: Date,
   endDate: Date,
-  user: User
+  user: User,
+  category: Category
 ) => {
-  //Todo Add the return Logic
+  const firstDate = date.format(startDate, "yyyy-MM-dd");
+  const lastDate = date.format(endDate, "yyyy-MM-dd");
+  const test = await repo.find({
+    where: {
+      // user: { id: user.id },
+      date: Between(lastDate as any, firstDate as any),
+      // category: { id: category.id },
+    },
+  });
+  console.log(test);
+  // return repo.sum("amount", {
+  //   user: { id: user.id },
+  //   date: Between(startDate, endDate),
+  //   category: { id: category.id },
+  // });
+  return 0;
+};
+
+export const getFilteredExpenses = (user: User, params: ExpenseQuery) => {
+  const whereConditions: FindOptionsWhere<Expense> = { user: { id: user.id } };
+  if (params.id) whereConditions.id = params.id;
+  if (params.amount) whereConditions.amount = params.amount;
+  if (params.date) whereConditions.date = params.date;
+  if (params.description)
+    whereConditions.description = ILike(`%${params.description}%`);
+  if (params.category) whereConditions.category = { id: params.category };
+  return repo.find({ where: whereConditions });
 };
