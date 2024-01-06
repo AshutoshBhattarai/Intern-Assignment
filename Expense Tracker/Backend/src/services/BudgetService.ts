e8f6dbaab079cda372ab47ac408932eebcf7eb71
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-06 14:46:18 +0545

    Update budget limit check

--- Expense Tracker/Backend/src/services/BudgetService.ts
+++ Expense Tracker/Backend/src/services/BudgetService.ts
@@ -1,16 +1,23 @@
 import NotFoundError from "../errors/NotFound";
+import WarningError from "../errors/Warning";
 import Budget from "../models/Budget";
 import User from "../models/User";
 import * as budgetRepo from "../repositories/BudgetRepo";
 import * as categoryRepo from "../repositories/CategoryRepo";
+import { totalIncome } from "../repositories/IncomeRepo";
 import * as userRepo from "../repositories/UserRepo";
 
 export const createBudget = async (user: User, budget: Budget) => {
   if (!(await userRepo.getUserById(user.id)))
     throw new NotFoundError("User not found");
-  if (!await categoryRepo.getCategory(budget.category as any))
+  if (!(await categoryRepo.getCategory(budget.category as any)))
     throw new NotFoundError("Category not found");
   budget.user = user;
+
+  const availableBudget = (await totalIncome(user)) || 0;
+  const allocatedBudget = (await budgetRepo.getTotalBudget(user)) || 0;
+  if (availableBudget < budget.amount + allocatedBudget)
+    throw new WarningError("Amount Exceeds Your Total Budget");
   const newBudget = await budgetRepo.createBudget(budget);
   return newBudget;
 };
