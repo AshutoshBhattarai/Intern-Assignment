d57cf08e7ff45ee690db8985a93ecb65fd28ba6c
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-06 11:09:13 +0545

    Update: Add Routes

--- Expense Tracker/Backend/src/routes/Expense.ts
+++ Expense Tracker/Backend/src/routes/Expense.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 const router = Router();
+import {
+  createExpense,
+  getAllExpenses,
+  getFilteredExpenses,
+} from "../controllers/ExpenseController";
 
-router.route("/").get().post();
-router.route("/:id").get().patch().delete();
+router.route("/").get(getAllExpenses).post(createExpense).patch().delete();
+router.route("/filter").get(getFilteredExpenses);
 
 export default router;
