e10ad880d42892f69d1f9b1f29ac15dbc8374092
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-06 14:47:18 +0545

    Update add expensse with budget check

--- Expense Tracker/Backend/src/services/ExpenseService.ts
+++ Expense Tracker/Backend/src/services/ExpenseService.ts
@@ -1,11 +1,22 @@
 import NotFoundError from "../errors/NotFound";
 import Expense from "../models/Expense";
 import User from "../models/User";
+import { getCategory } from "../repositories/CategoryRepo";
 import * as expenseRepo from "../repositories/ExpenseRepo";
 import { getUserById } from "../repositories/UserRepo";
+import { ExpenseQuery } from "../types/QueryType";
+import * as budgetRepo from "../repositories/BudgetRepo";
+import WarningError from "../errors/Warning";
 
 export const createExpense = async (user: User, expense: Expense) => {
   if (!(await getUserById(user.id))) throw new NotFoundError("User not found");
+  const category = await getCategory(expense.category as any);
+  if (!category) throw new NotFoundError("Category not found");
+  const totalBudget =
+    (await budgetRepo.getCategoryTotalBudget(user, category)) || 0;
+  if (totalBudget < expense.amount) {
+    throw new WarningError("Amount Exceeds Your Total Budget");
+  }
   expense.user = user;
   expenseRepo.createExpense(expense);
 };
@@ -15,6 +26,9 @@ export const getAllExpenses = async (user: User) => {
   return expenseRepo.getExpenses(user);
 };
 
-export const updateExpense = async (user: User, expense: Expense) =>{
-    
-}
+export const updateExpense = async (user: User, expense: Expense) => {};
+
+export const getFilteredExpenses = async (
+  user: User,
+  params: ExpenseQuery
+) => {};
