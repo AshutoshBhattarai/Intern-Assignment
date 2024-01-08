276023ab7a4eafb10570a26da38fbdddb8a1bde4
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-08 08:07:31 +0545

    Create all pages with navbar

---
+++ Expense Tracker/Frontend/src/views/Profile/script.ts
@@ -0,0 +1,6 @@
+import renderNavBar from "../../components/Navbar/navbar";
+import "../../assets/scss/style.scss";
+const navBar = document.getElementById("nav-placeholder") as HTMLElement;
+window.onload = () => {
+  renderNavBar(navBar, "nav-profile");
+};
