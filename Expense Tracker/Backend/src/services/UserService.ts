d13ee3c72481d8654b055ab68c3873476a4bdc79
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-08 11:02:49 +0545

    Add summary logic

--- Expense Tracker/Backend/src/services/UserService.ts
+++ Expense Tracker/Backend/src/services/UserService.ts
@@ -1,6 +1,11 @@
 import NotFoundError from "../errors/NotFound";
 import User from "../models/User";
 import * as userRepo from "../repositories/UserRepo";
+import * as categoryRepo from "../repositories/CategoryRepo";
+import * as expenseRepo from "../repositories/ExpenseRepo";
+import * as budgetRepo from "../repositories/BudgetRepo";
+import * as incomeRepo from "../repositories/IncomeRepo";
+import { UserSummary } from "../types/UserSummary";
 
 export const getAllUsers = async (): Promise<User[]> => {
   const users = await userRepo.getAllUsers();
@@ -28,6 +33,28 @@ export const deleteUser = async (id: string) => {
 export const updateUser = async (id: string) => {
   const user = await userRepo.getUserById(id);
   if (!user) throw new NotFoundError("User not found");
+};
+export const getUserSummary = async (user: User) => {
+  const foundUser = await userRepo.getUserById(user.id);
+  if (!foundUser) throw new NotFoundError("User not found");
+  const totalIncome = await incomeRepo.getUserTotalIncome(foundUser.id);
+  const totalExpense = await expenseRepo.getUserTotalExpense(foundUser.id);
+  const totalBudget = await budgetRepo.getUserTotalBudget(foundUser.id);
+  // Returns array where first element is array of expense and the second element is count
+  const expenseCount = await expenseRepo.getUserExpenseCount(foundUser.id);
+
+  const summary: UserSummary = {};
+  summary.id = foundUser.id;
+  summary.email = foundUser.email;
+  summary.username = foundUser.username;
+  summary.totalIncome = totalIncome || 0;
+  summary.totalExpense = totalExpense || 0;
+  summary.totalBudget = totalBudget || 0;
+  
+  // expenseCount[1] is the count
+  summary.countExpense = expenseCount[1]!;
+
+  return summary;
 };
 
 const filterUser = (user: User) => {
