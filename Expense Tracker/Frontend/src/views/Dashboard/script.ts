78a32d155fd7e2b97a688101db2460326de5ab13
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-08 11:03:43 +0545

    Add user summary

--- Expense Tracker/Frontend/src/views/Dashboard/script.ts
+++ Expense Tracker/Frontend/src/views/Dashboard/script.ts
@@ -1,10 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { HttpStatusCode } from "axios";
 import "../../assets/scss/style.scss";
 import renderNavBar from "../../components/Navbar/navbar";
-import { Chart } from "chart.js/auto";
 import http from "../../service/HttpClient";
 const navBar = document.getElementById("navbar-placeholder") as HTMLElement;
+const displayName = document.getElementById("display-username") as HTMLElement;
+const displayIncome = document.getElementById("summary-income") as HTMLElement;
+const displayExpense = document.getElementById(
+  "summary-expense"
+) as HTMLElement;
+const displayBudget = document.getElementById("summary-budget") as HTMLElement;
 const expenseChart = document.getElementById(
   "expense-chart"
 ) as HTMLCanvasElement;
@@ -13,38 +19,18 @@ const expenseContext = expenseChart.getContext(
 ) as CanvasRenderingContext2D;
 window.onload = async () => {
   renderNavBar(navBar, "nav-dashboard");
-  const expenseResponse = await http.get("/expenses", {
+  const userResponse = await http.get("/users/summary", {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("jwt")}`,
     },
   });
-  const userResponse = await http.get("/users", {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-    },
-  });
-  console.log(userResponse);
-
-  const expenses = expenseResponse.data.data;
-  const expenseLabels = expenses.map((expense: any) => expense.date);
-  const expenseAmount = expenses.map((expense: any) => expense.amount);
-  const myChart = new Chart(expenseContext, {
-    type: "bar",
-    data: {
-      labels: expenseLabels,
-      datasets: [
-        {
-          label: "Amount Spent",
-          data: expenseAmount,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
+  const userSummary = userResponse.data.data;
+  if (userResponse.status === HttpStatusCode.Ok) {
+    displayName.innerHTML = userSummary.username;
+    displayIncome.innerHTML = `Rs.${userSummary.totalIncome}`;
+    displayExpense.innerHTML = `Rs.${userSummary.totalExpense}`;
+    displayBudget.innerHTML = `Rs.${
+      userSummary.totalIncome - userSummary.totalExpense
+    }`;
+  }
 };
