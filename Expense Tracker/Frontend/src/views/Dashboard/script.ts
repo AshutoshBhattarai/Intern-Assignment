ee40ea5da66dc71746b5f27ec17b65487728b292
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-08 08:07:54 +0545

    update dashboard with chart

--- Expense Tracker/Frontend/src/views/Dashboard/script.ts
+++ Expense Tracker/Frontend/src/views/Dashboard/script.ts
@@ -1,7 +1,50 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import "../../assets/scss/style.scss";
 import renderNavBar from "../../components/Navbar/navbar";
+import { Chart } from "chart.js/auto";
+import http from "../../service/HttpClient";
 const navBar = document.getElementById("navbar-placeholder") as HTMLElement;
-window.onload = () => {
+const expenseChart = document.getElementById(
+  "expense-chart"
+) as HTMLCanvasElement;
+const expenseContext = expenseChart.getContext(
+  "2d"
+) as CanvasRenderingContext2D;
+window.onload = async () => {
   renderNavBar(navBar, "nav-dashboard");
+  const expenseResponse = await http.get("/expenses", {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+    },
+  });
+  const userResponse = await http.get("/users", {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+    },
+  });
+  console.log(userResponse);
+
+  const expenses = expenseResponse.data.data;
+  const expenseLabels = expenses.map((expense: any) => expense.date);
+  const expenseAmount = expenses.map((expense: any) => expense.amount);
+  const myChart = new Chart(expenseContext, {
+    type: "bar",
+    data: {
+      labels: expenseLabels,
+      datasets: [
+        {
+          label: "Amount Spent",
+          data: expenseAmount,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
 };
