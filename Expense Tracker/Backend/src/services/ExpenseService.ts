9ecc00849fa573465a04c37d8ba2b17cb12f3eb9
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-09 17:16:25 +0545

    Update buget on adding expense

--- Expense Tracker/Backend/src/services/ExpenseService.ts
+++ Expense Tracker/Backend/src/services/ExpenseService.ts
@@ -1,12 +1,14 @@
 import NotFoundError from "../errors/NotFound";
+import WarningError from "../errors/Warning";
+import Budget from "../models/Budget";
+import Category from "../models/Category";
 import Expense from "../models/Expense";
 import User from "../models/User";
+import * as budgetRepo from "../repositories/BudgetRepo";
 import { getCategory } from "../repositories/CategoryRepo";
 import * as expenseRepo from "../repositories/ExpenseRepo";
 import { getUserById } from "../repositories/UserRepo";
 import { ExpenseQuery } from "../types/QueryType";
-import * as budgetRepo from "../repositories/BudgetRepo";
-import WarningError from "../errors/Warning";
 
 //* -------------------- Function that adds a new expense -------------------- */
 
@@ -22,35 +24,23 @@ export const createExpense = async (user: User, expense: Expense) => {
     throw new NotFoundError("Category not found");
   }
 
-  // Get the total budget for the category
-  const totalBudget =
-    (await budgetRepo.getCategoryTotalBudget(user, category)) || 0;
+  const [budget]: Budget[] = await budgetRepo.getBudgetByCategory(
+    user,
+    category
+  );
 
-  // Get the start and end dates of the current month
-  const date = new Date();
-  const startDate = new Date(date.getFullYear(), date.getMonth(), 1);
-  const endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-
-  // Get the total expense for the current month
-  //! Currently Doesn't Work (Total Expense is just 0 for now)
-  const totalExpense =
-    (await expenseRepo.getTotalExpenseByDate(
-      startDate,
-      endDate,
-      user,
-      category
-    )) || 0;
-  console.log(totalExpense);
-
-  // Calculate the remaining budget
-  const remainingBudget = totalBudget - totalExpense;
-
-  // Check if the remaining budget is enough to cover the expense amount
-  if (remainingBudget < expense.amount) {
-    throw new WarningError("Amount Exceeds Your Budget");
+  if (!budget) {
+    throw new NotFoundError("Budget for this category doesn't exist found");
   }
+  if (budget.remainingAmount < expense.amount) {
+    throw new WarningError("Amount Exceeds Your Remaining Budget");
+  }
+  const updateBudget = new Budget();
+  updateBudget.id = budget.id;
+  updateBudget.spentAmount = budget.spentAmount + expense.amount;
+  updateBudget.remainingAmount = budget.amount - updateBudget.spentAmount;
+  await budgetRepo.updateBudget(updateBudget);
 
-  // Set the user for the expense
   expense.user = user;
 
   // Create the expense
@@ -59,28 +49,36 @@ export const createExpense = async (user: User, expense: Expense) => {
 //* ----------------------------------- -- ----------------------------------- */
 //* ------- Service that returns all the expenses of a particular user ------ */
 export const getAllExpenses = async (user: User) => {
-  if (!(await getUserById(user.id))) throw new NotFoundError("User not found");
+  if (!(await getUserById(user.id))) {
+    throw new NotFoundError("User not found");
+  }
   const expenses = await expenseRepo.getExpenses(user);
   return expenses.map((expense) => expenseResponse(expense));
 };
 //* ----------------------------------- -- ----------------------------------- */
 /* --------------------- Service that updates an expense -------------------- */
 export const updateExpense = async (user: User, expense: Expense) => {
-  if (!(await getUserById(user.id))) throw new NotFoundError("User not found");
+  if (!(await getUserById(user.id))) {
+    throw new NotFoundError("User not found");
+  }
   expense.user = user;
   expenseRepo.updateExpense(expense);
 };
 //* ----------------------------------- -- ----------------------------------- */
 //* -------------- Service filteres expenses by query parameters ------------- */
 export const getFilteredExpenses = async (user: User, params: ExpenseQuery) => {
-  if (!(await getUserById(user.id))) throw new NotFoundError("User not found");
-  const expenses = await expenseRepo.getFilteredExpenses(user, params);
+  if (!(await getUserById(user.id))) {
+    throw new NotFoundError("User not found");
+  }
+  // const expenses = await expenseRepo.getFilteredExpenses(user, params);
+  const expenses = await expenseRepo.getExpenseWithCategory(user, params);
   return expenses.map((expense) => expenseResponse(expense));
 };
 /* ----------------------------------- -- ----------------------------------- */
-
 export const deleteExpense = async (user: User, id: string) => {
-  if (!(await getUserById(user.id))) throw new NotFoundError("User not found");
+  if (!(await getUserById(user.id))) {
+    throw new NotFoundError("User not found");
+  }
   const expense = await expenseRepo.getExpensesById(id);
   if (!expense) throw new NotFoundError("Expense not found");
   expenseRepo.deleteExpense(expense.id);
@@ -96,5 +94,9 @@ const expenseResponse = (expense: Expense) => {
   resExpense.image = `http://localhost:5000/images/${expense.user}/${
     expense.image || "no-image"
   }`;
+  const category = new Category();
+  category.id = expense.category.id;
+  category.title = expense.category.title;
+  resExpense.category = category;
   return resExpense;
 };
