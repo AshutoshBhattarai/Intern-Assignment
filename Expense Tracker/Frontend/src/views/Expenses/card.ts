967a7333201d42945cd08d0104fc1bb917805912
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-12 20:09:35 +0545

    Add filters seperated card

---
+++ Expense Tracker/Frontend/src/views/Expenses/card.ts
@@ -0,0 +1,84 @@
+import * as bootstrap from "bootstrap";
+import Category from "../../interfaces/Category";
+import Expense from "../../interfaces/Expense";
+
+const createExpenseCard = (
+  expense: Expense,
+  editOnClick: () => void,
+  deleteOnClick: () => void
+) => {
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add(
+    "row",
+    "border",
+    "rounded-3",
+    "py-2",
+    "mb-2",
+    "card"
+  );
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("row", "m-0", "align-items-center", "col-12");
+
+  const amount = document.createElement("div");
+  amount.classList.add("col-1", "text-danger", "m-0", "align-middle");
+  amount.innerHTML = `-Rs. ${expense.amount}`;
+
+  const remarks = document.createElement("div");
+  remarks.classList.add("col-6");
+  remarks.textContent = expense.description;
+
+  const category = document.createElement("div");
+  category.classList.add("col");
+  category.textContent = (expense.category as Category).title;
+
+  const date = document.createElement("div");
+  date.classList.add("col");
+  date.textContent = new Date(expense.date as Date)
+    .toUTCString()
+    .substring(5, 16);
+
+  const actions = document.createElement("div");
+  actions.classList.add("col");
+
+  const editButton = document.createElement("button");
+  editButton.innerHTML = "<i class='fa-solid fa-pen-to-square'></i>";
+  editButton.classList.add("btn", "btn-outline-primary", "col");
+  editButton.addEventListener("click", editOnClick);
+  editButton.setAttribute("data-bs-toggle", "tooltip");
+  editButton.setAttribute("data-bs-placement", "top");
+  editButton.setAttribute("data-bs-title", "Edit Expense");
+  new bootstrap.Tooltip(editButton);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "<i class='fa-solid fa-trash'></i>";
+  deleteButton.classList.add("btn", "btn-outline-danger", "mx-2", "col");
+  deleteButton.addEventListener("click", deleteOnClick);
+  deleteButton.setAttribute("data-bs-toggle", "tooltip");
+  deleteButton.setAttribute("data-bs-placement", "top");
+  deleteButton.setAttribute("data-bs-title", "Delete Expense");
+  new bootstrap.Tooltip(deleteButton);
+
+  const viewImageButton = document.createElement("a");
+  viewImageButton.innerHTML = "<i class='fa-solid fa-receipt'></i>";
+  viewImageButton.classList.add("btn", "btn-outline-warning", "col");
+  viewImageButton.href = expense.image || "#";
+  viewImageButton.target = "_blank";
+  viewImageButton.setAttribute("data-bs-toggle", "tooltip");
+  viewImageButton.setAttribute("data-bs-placement", "top");
+  viewImageButton.setAttribute("data-bs-title", "View Receipt");
+  new bootstrap.Tooltip(viewImageButton);
+
+  cardBody.appendChild(amount);
+  cardBody.appendChild(remarks);
+  cardBody.appendChild(category);
+  cardBody.appendChild(date);
+  actions.appendChild(editButton);
+  actions.appendChild(deleteButton);
+  actions.appendChild(viewImageButton);
+  cardBody.appendChild(actions);
+  cardContainer.appendChild(cardBody);
+  return cardContainer;
+};
+
+export default createExpenseCard;
--- Expense Tracker/Frontend/src/views/Expenses/script.ts
+++ Expense Tracker/Frontend/src/views/Expenses/script.ts
@@ -2,7 +2,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { HttpStatusCode } from "axios";
 import * as bootstrap from "bootstrap";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import moment from "moment";
 import "../../assets/scss/style.scss";
+import generateExpenseCard from "./card";
 import renderNavBar from "../../components/Navbar/navbar";
 import Category from "../../interfaces/Category";
 import Expense from "../../interfaces/Expense";
@@ -15,7 +19,9 @@ import { showToast } from "../../components/Toast";
 import showErrorResponse from "../../service/ErrorResponse";
 /* ------------------------ Getting elements from DOM ----------------------- */
 const navBar = document.getElementById("nav-placeholder") as HTMLElement;
-const searchInput = document.getElementById("search-bar") as HTMLInputElement;
+const searchInput = document.getElementById(
+  "filter-search"
+) as HTMLInputElement;
 const searchBtn = document.getElementById("btn-search") as HTMLElement;
 const expenseAddBtn = document.getElementById("btn-add-expense") as HTMLElement;
 const btnSaveExpense = document.getElementById(
@@ -34,30 +40,78 @@ const categoryInput = document.getElementById(
 ) as HTMLInputElement;
 const receiptInput = document.getElementById("add-receipt") as HTMLInputElement;
 const dateInput = document.getElementById("add-date") as HTMLInputElement;
-
 const expensesContainer = document.getElementById(
   "expense-container"
 ) as HTMLElement;
-
+const filterAmountSlider = document.getElementById(
+  "filter-amount"
+) as HTMLInputElement;
 const toastContainer = document.getElementById("toast-message") as HTMLElement;
 
 let expenseModal: bootstrap.Modal;
 let dialogExpenseId: string = "";
+let searchParams = "";
 /* ----------------------------------- -- ----------------------------------- */
 window.onload = async () => {
   renderNavBar(navBar, "nav-expenses");
   renderExpenseCards("");
   expenseModal = new bootstrap.Modal(addDialogBox);
   createCategoryOptions(categoryInput);
+  new bootstrap.Dropdown(
+    document.getElementById("filter-category-dropdown") as HTMLElement
+  );
+  const categories = await createGetRequest("/categories/");
+  const dropDownMenu = document.getElementById(
+    "filter-category-menu"
+  ) as HTMLElement;
+  const btnAll = document.createElement("button");
+  btnAll.classList.add("dropdown-item");
+  btnAll.textContent = "All";
+  btnAll.addEventListener("click", () => {
+    searchParams = "";
+    renderExpenseCards(searchParams);
+  });
+  dropDownMenu.appendChild(btnAll);
+  categories.forEach((category: Category) => {
+    const btn = document.createElement("button");
+    btn.classList.add("dropdown-item");
+    btn.textContent = category.title;
+    btn.textContent = category.title;
+    btn.addEventListener("click", () => {
+      searchParams += `&category=${category.id?.toString()}`;
+      renderExpenseCards(searchParams);
+      searchParams = "";
+    });
+
+    dropDownMenu.appendChild(btn);
+  });
+  flatpickr("#filter-date", {
+    mode: "range",
+    dateFormat: "YYYY-MM-DD",
+    onClose: function (selectedDates, _dateStr, instance) {
+      if (selectedDates.length === 2) {
+        const startDate = moment(selectedDates[0]).format("YYYY-MM-DD");
+        const endDate = moment(selectedDates[1]).format("YYYY-MM-DD");
+        instance.input.value = `${startDate} to ${endDate}`;
+        renderExpenseCards(`&startDate=${startDate}&endDate=${endDate}`);
+      }
+    },
+  });
 };
 
 searchBtn.addEventListener("click", () => {
   const searchData = searchInput.value;
-  console.log(searchData);
+  searchData
+    ? renderExpenseCards(`&description=${searchData}`)
+    : renderExpenseCards("");
 });
 
 expenseAddBtn.addEventListener("click", () => {
   showDialog();
+});
+
+filterAmountSlider.addEventListener("change", () => {
+  console.log(filterAmountSlider.value);
 });
 
 btnCloseDialog.addEventListener("click", () => {
@@ -89,7 +143,6 @@ const saveExpense = async () => {
   formData.append("category", category);
   receipt && formData.append("image", receipt);
   formData.append("date", date.toISOString());
-  
 
   if (dialogExpenseId === "") {
     createExpense(formData);
@@ -121,70 +174,110 @@ const showDialog = (data?: {
   dateInput.value = data?.date.toString() || "";
 };
 const createExpenseCard = (data: Expense) => {
-  console.log(dialogExpenseId);
-  // Create the expense card element
-  const expenseCard = document.createElement("div");
-  expenseCard.classList.add("card", "mb-2", "col-md-8", "gx-3");
-  expenseCard.style.position = "relative"; // Set position relative for absolute positioning
+  // console.log(dialogExpenseId);
+  // // Create the expense card element
+  // const expenseCard = document.createElement("div");
+  // expenseCard.classList.add("card", "mb-2", "col-md-8", "gx-3");
+  // expenseCard.style.position = "relative"; // Set position relative for absolute positioning
 
-  // Create the card body element
-  const cardBody = document.createElement("div");
-  cardBody.classList.add("card-body", "mr-3");
+  // // Create the card body element
+  // const cardBody = document.createElement("div");
+  // cardBody.classList.add("card-body", "mr-3");
 
-  // Create the card description element and assign the remarks from the data
-  const cardDescription = document.createElement("p");
-  cardDescription.classList.add("card-title", "text-primary", "m-0");
-  cardDescription.textContent = data.description;
+  // // Create the card description element and assign the remarks from the data
+  // const cardDescription = document.createElement("p");
+  // cardDescription.classList.add("card-title", "text-primary", "m-0");
+  // cardDescription.textContent = data.description;
 
-  // Create the card amount element and assign the amount from the data
-  const cardAmount = document.createElement("h4");
-  cardAmount.classList.add("card-text", "text-danger", "m-0");
-  cardAmount.innerHTML = "<i class='fas fa-indian-rupee-sign'></i> " + data.amount;
+  // // Create the card amount element and assign the amount from the data
+  // const cardAmount = document.createElement("p");
+  // cardAmount.classList.add("card-text", "text-danger", "m-0");
+  // cardAmount.innerHTML = "<i class='fas fa-rupee'></i> " + data.amount;
 
-  const cardCategory = document.createElement("p");
-  cardCategory.classList.add("card-text", "m-0");
-  cardCategory.textContent = (data.category as Category).title;
+  // const cardCategory = document.createElement("p");
+  // cardCategory.classList.add("card-text", "m-0");
+  // cardCategory.textContent = (data.category as Category).title;
 
-  // Create the card date element and assign the date from the data
-  const cardDate = document.createElement("p");
-  cardDate.classList.add("card-text");
-  cardDate.textContent = new Date(data.date as Date)
-    .toUTCString()
-    .substring(5, 16);
+  // // Create the card date element and assign the date from the data
+  // const cardDate = document.createElement("p");
+  // cardDate.classList.add("card-text");
+  // cardDate.textContent = new Date(data.date as Date)
+  //   .toUTCString()
+  //   .substring(5, 16);
 
-  // Create the delete button container with absolute positioning
-  const btnDeleteContainer = document.createElement("div");
-  btnDeleteContainer.style.position = "absolute";
-  btnDeleteContainer.style.top = "5px";
-  btnDeleteContainer.style.right = "20px";
+  // // Create the delete button container with absolute positioning
+  // const btnDeleteContainer = document.createElement("div");
+  // btnDeleteContainer.style.position = "absolute";
+  // btnDeleteContainer.style.top = "5px";
+  // btnDeleteContainer.style.right = "20px";
 
-  // Create the delete button element and add a click event listener
-  const btnDelete = document.createElement("button");
-  btnDelete.classList.add("btn", "btn-outline-danger", "round");
-  btnDelete.innerHTML = "<i class='fas fa-trash'></i>";
-  btnDelete.setAttribute("data-bs-toggle", "tooltip");
-  btnDelete.setAttribute("data-bs-placement", "top");
-  btnDelete.setAttribute("data-bs-title", "Delete Expense");
-  const btnDeletetooltip = new bootstrap.Tooltip(btnDelete);
-  btnDelete.addEventListener("click", () => {
+  // // Create the delete button element and add a click event listener
+  // const btnDelete = document.createElement("button");
+  // btnDelete.classList.add("btn", "btn-outline-danger", "round");
+  // btnDelete.innerHTML = "<i class='fas fa-trash'></i>";
+  // btnDelete.setAttribute("data-bs-toggle", "tooltip");
+  // btnDelete.setAttribute("data-bs-placement", "top");
+  // btnDelete.setAttribute("data-bs-title", "Delete Expense");
+  // const btnDeletetooltip = new bootstrap.Tooltip(btnDelete);
+  // btnDelete.addEventListener("click", () => {
+  //   deleteExpense(data.id!);
+  //   btnDeletetooltip.dispose();
+  // });
+
+  // // Append the delete button to its container
+  // btnDeleteContainer.appendChild(btnDelete);
+
+  // // Create the edit button container with absolute positioning
+  // const btnEditContainer = document.createElement("div");
+  // btnEditContainer.style.position = "absolute";
+  // btnEditContainer.style.top = "5px";
+  // btnEditContainer.style.right = "70px"; // Adjust the right value as needed
+
+  // // Create the edit button element and add a click event listener
+  // const btnEdit = document.createElement("button");
+  // btnEdit.classList.add("btn", "btn-outline-primary");
+  // btnEdit.innerHTML = "<i class='fas fa-edit'></i>";
+  // btnEdit.addEventListener("click", () => {
+  //   dialogExpenseId = data.id!;
+  //   return showDialog({
+  //     remarks: data.description,
+  //     amount: data.amount!,
+  //     date: data.date as string,
+  //     category: (data.category as Category).id!,
+  //   });
+  // });
+  // btnEdit.setAttribute("data-bs-toggle", "tooltip");
+  // btnEdit.setAttribute("data-bs-placement", "top");
+  // btnEdit.setAttribute("data-bs-title", "Edit Expense");
+  // new bootstrap.Tooltip(btnEdit);
+  // // Append the edit button to its container
+  // btnEditContainer.appendChild(btnEdit);
+  // // Create the view image link element and set the href and target
+  // const viewImage = document.createElement("a");
+  // viewImage.classList.add("btn", "btn-outline-warning");
+  // viewImage.innerHTML = "<i class='fa-solid fa-file-invoice'></i>";
+  // viewImage.href = data.image || "#";
+  // viewImage.target = "_blank";
+  // viewImage.setAttribute("data-bs-toggle", "tooltip");
+  // viewImage.setAttribute("data-bs-placement", "top");
+  // viewImage.setAttribute("data-bs-title", "View Receipt");
+  // new bootstrap.Tooltip(viewImage);
+
+  // // Append the card date, description, amount, delete button container, edit button, and view image link to the card body
+  // cardBody.appendChild(cardAmount);
+  // cardBody.appendChild(cardDescription);
+  // cardBody.appendChild(cardCategory);
+  // cardBody.appendChild(cardDate);
+  // cardBody.appendChild(btnDeleteContainer);
+  // cardBody.appendChild(btnEditContainer);
+  // cardBody.appendChild(viewImage);
+
+  // // Append the card body to the expense card
+  // expenseCard.appendChild(cardBody);
+  const deleteFunction = () => {
     deleteExpense(data.id!);
-    btnDeletetooltip.dispose();
-  });
-
-  // Append the delete button to its container
-  btnDeleteContainer.appendChild(btnDelete);
-
-  // Create the edit button container with absolute positioning
-  const btnEditContainer = document.createElement("div");
-  btnEditContainer.style.position = "absolute";
-  btnEditContainer.style.top = "5px";
-  btnEditContainer.style.right = "70px"; // Adjust the right value as needed
-
-  // Create the edit button element and add a click event listener
-  const btnEdit = document.createElement("button");
-  btnEdit.classList.add("btn", "btn-outline-primary");
-  btnEdit.innerHTML = "<i class='fas fa-edit'></i>";
-  btnEdit.addEventListener("click", () => {
+  };
+  const editFunction = () => {
     dialogExpenseId = data.id!;
     return showDialog({
       remarks: data.description,
@@ -192,37 +285,18 @@ const createExpenseCard = (data: Expense) => {
       date: data.date as string,
       category: (data.category as Category).id!,
     });
-  });
-  btnEdit.setAttribute("data-bs-toggle", "tooltip");
-  btnEdit.setAttribute("data-bs-placement", "top");
-  btnEdit.setAttribute("data-bs-title", "Edit Expense");
-  new bootstrap.Tooltip(btnEdit);
-  // Append the edit button to its container
-  btnEditContainer.appendChild(btnEdit);
-  // Create the view image link element and set the href and target
-  const viewImage = document.createElement("a");
-  viewImage.classList.add("btn", "btn-primary");
-  viewImage.textContent = "View Receipt";
-  viewImage.href = data.image || "#";
-  viewImage.target = "_blank";
-
-  // Append the card date, description, amount, delete button container, edit button, and view image link to the card body
-  cardBody.appendChild(cardAmount);
-  cardBody.appendChild(cardDescription);
-  cardBody.appendChild(cardCategory);
-  cardBody.appendChild(cardDate);
-  cardBody.appendChild(btnDeleteContainer);
-  cardBody.appendChild(btnEditContainer);
-  cardBody.appendChild(viewImage);
-
-  // Append the card body to the expense card
-  expenseCard.appendChild(cardBody);
-
+  };
+  const viewImageFunc = () => {};
+  const expenseCard = generateExpenseCard(
+    data,
+    editFunction,
+    deleteFunction
+  );
   // Return the created expense card element
   return expenseCard;
 };
 
-const renderExpenseCards = async (filter: any) => {
+const renderExpenseCards = async (filter: string) => {
   const userExpenses = await createGetRequest(`/expenses/filter?${filter}`);
   if (userExpenses.length == 0) {
     expensesContainer.innerHTML =
@@ -231,7 +305,7 @@ const renderExpenseCards = async (filter: any) => {
     return;
   }
   expensesContainer.innerHTML = "";
-  userExpenses.forEach((expense: any) => {
+  userExpenses.forEach((expense: Expense) => {
     expensesContainer.appendChild(createExpenseCard(expense));
   });
 };
