dc4b9c94dbb1f3fc0f3f364a64725f2410460557
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-12 20:08:33 +0545

    Update navbar

--- Expense Tracker/Frontend/src/components/Navbar/navbar.ts
+++ Expense Tracker/Frontend/src/components/Navbar/navbar.ts
@@ -1,3 +1,4 @@
+import * as bootstrap from "bootstrap";
 const renderNavBar = (placeholder: HTMLElement, active: string) => {
   fetch("../../components/Navbar/navbar.html")
     .then((response) => response.text())
@@ -5,10 +6,26 @@ const renderNavBar = (placeholder: HTMLElement, active: string) => {
       placeholder.innerHTML = data;
       const navLinks = document.querySelectorAll(".nav-link");
       const logout = document.getElementById("btn-logout");
-
+      const profileDropDownContainer = document.getElementById(
+        "dropdown-container"
+      ) as HTMLElement;
+      const dropDown = new bootstrap.Dropdown(profileDropDownContainer);
       logout?.addEventListener("click", () => {
         localStorage.removeItem("jwt");
         window.location.href = "/views/login";
+      });
+
+      const profileDropDown = document.getElementById(
+        "profile-dropdown"
+      ) as HTMLElement;
+      profileDropDown.addEventListener("click", () => {
+        dropDown.show();
+      });
+      profileDropDown.addEventListener("mouseenter", () => {
+        dropDown.show();
+      });
+      profileDropDown.addEventListener("mouseleave", () => {
+        dropDown.hide();
       });
 
       for (const navLink of navLinks) {
