2451e446824e927e3e7108e5b2d6aef9cd427164
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-13 08:47:55 +0545

    Change type to interface

--- Expense Tracker/Backend/src/interface/QueryInterface.ts
+++ Expense Tracker/Backend/src/interface/QueryInterface.ts
@@ -1,27 +1,27 @@
-export type BudgetQuery = {
-  id?: string;
+interface Query {
+  page?: number;
+  id ?: string;
+}
+export interface BudgetQuery extends Query {
   title?: string;
   startDate?: Date;
   endDate?: Date;
   category?: string;
-};
+}
 
-export type CategoryQuery = {
-  id?: string;
+export interface CategoryQuery extends Query {
   title?: string;
 };
 
-export type IncomeQuery = {
-  id?: number;
+export interface IncomeQuery  {
   source?: string;
   amount?: number;
 };
 
-export type ExpenseQuery = {
-  id?: string;
+export interface ExpenseQuery extends Query {
   amount?: number;
   startDate?: Date;
   endDate?: Date;
   description?: string;
   category?: string;
-};
+}
--- Expense Tracker/Backend/src/interface/UserSummary.ts
+++ Expense Tracker/Backend/src/interface/UserSummary.ts
@@ -1,4 +1,4 @@
-export type UserSummary = {
+export interface UserSummary {
   id?: string;
   username?: string;
   email?: string;
