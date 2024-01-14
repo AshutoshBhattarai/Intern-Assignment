c98734df61eac1fa1830cb1fbb71eca96325f60d
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-14 08:48:40 +0545

    fix cache issue

--- Expense Tracker/Frontend/src/service/HttpClient.ts
+++ Expense Tracker/Frontend/src/service/HttpClient.ts
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const http = axios.create({
   baseURL: "http://localhost:5000",
+  headers: {
+    "Cache-Control": "no-cache",
+    Pragma: "no-cache",
+    Expires: "0",
+  },
 });
 
 export default http;
