bce37de50bc573e44b05e148781dfda1e1e6c950
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-14 15:26:17 +0545

    Update add charts

--- Expense Tracker/Frontend/src/views/Dashboard/script.ts
+++ Expense Tracker/Frontend/src/views/Dashboard/script.ts
@@ -1,10 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { HttpStatusCode } from "axios";
+import Chart from "chart.js/auto";
+import "chartjs-adapter-moment";
 import "../../assets/scss/style.scss";
 import renderNavBar from "../../components/Navbar/navbar";
-import http from "../../service/HttpClient";
+import UserSummary from "../../interfaces/UserSummary";
 import createGetRequest from "../../service/GetRequest";
+/* -------------------------------------------------------------------------- */
+/*                          Getting elements from DOM                         */
+/* -------------------------------------------------------------------------- */
 const navBar = document.getElementById("navbar-placeholder") as HTMLElement;
 const displayName = document.getElementById("display-username") as HTMLElement;
 const displayIncome = document.getElementById("summary-income") as HTMLElement;
@@ -18,15 +20,98 @@ const expenseChart = document.getElementById(
 const expenseContext = expenseChart.getContext(
   "2d"
 ) as CanvasRenderingContext2D;
+const categoryChart = document.getElementById(
+  "category-chart"
+) as HTMLCanvasElement;
+const categoryContext = categoryChart.getContext(
+  "2d"
+) as CanvasRenderingContext2D;
+
 window.onload = async () => {
   renderNavBar(navBar, "nav-dashboard");
-  const userSummary = await createGetRequest("/users/summary");
+  const userSummaryResponse = await createGetRequest("/users/summary");
+  const userSummary: UserSummary = userSummaryResponse?.data;
   if (userSummary) {
-    displayName.innerHTML = userSummary.username;
-    displayIncome.innerHTML = `Rs.${userSummary.totalIncome}`;
-    displayExpense.innerHTML = `Rs.${userSummary.totalExpense}`;
-    displayBudget.innerHTML = `Rs.${
-      userSummary.totalIncome - userSummary.totalExpense
-    }`;
+    renderSummary(userSummary);
+    createExpenseChart(expenseContext, userSummary.totalExpenseByDate!);
+    createPieChart(categoryContext, userSummary.totalExpenseByCategory!);
   }
+};
+
+const renderSummary = (userSummary: UserSummary) => {
+  displayName.innerHTML = userSummary.username ? userSummary.username : "User";
+  displayIncome.innerHTML = `Rs.${userSummary.totalIncome?.toLocaleString()}`;
+  displayIncome.style.cursor = "pointer";
+  displayIncome.addEventListener("click", () => {
+    window.location.href = "/views/income/";
+  });
+  displayExpense.innerHTML = `Rs.${userSummary.totalExpense?.toLocaleString()}`;
+  displayExpense.style.cursor = "pointer";
+  displayExpense.addEventListener("click", () => {
+    window.location.href = "/views/expenses/";
+  });
+  displayBudget.innerHTML = `Rs.${(
+    userSummary.totalIncome! - userSummary.totalExpense!
+  ).toLocaleString()}`;
+};
+/* -------------------------------------------------------------------------- */
+/*                               Chart Section                                */
+/* -------------------------------------------------------------------------- */
+const createExpenseChart = (
+  ctx: CanvasRenderingContext2D,
+  data: UserSummary["totalExpenseByDate"]
+) => {
+  const dates = data!.map((item) => item.date);
+  const amounts = data!.map((item) => item.total);
+  // const counts = data!.map((item) => item.count);
+  new Chart(ctx, {
+    type: "bubble",
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: "Total Expenses",
+          data: amounts,
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+          yAxisID: "y-axis-1",
+        },
+      ],
+    },
+    options: {
+      scales: {
+        x: {
+          type: "time",
+          time: {
+            unit: "day",
+          },
+          title: {
+            display: true,
+            text: "Date",
+          },
+        },
+      },
+    },
+  });
+};
+const createPieChart = (
+  ctx: CanvasRenderingContext2D,
+  data: UserSummary["totalExpenseByCategory"]
+) => {
+  const amounts = data?.map((item) => item.total);
+  const labels = data?.map((item) => item.category);
+  new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          data: amounts,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {},
+  });
 };
