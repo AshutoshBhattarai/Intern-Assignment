2e3ba181ba1974b02271603954da305b99f9852f
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-14 17:15:39 +0545

    New search params

---
+++ Expense Tracker/Frontend/src/interfaces/SearchParams.ts
@@ -0,0 +1,12 @@
+interface SearchParams {
+  page?: number;
+  amount?: number;
+  category?: string;
+  status?: string;
+  startDate?: string;
+  endDate?: string;
+  source?: string;
+
+}
+
+export default SearchParams;
