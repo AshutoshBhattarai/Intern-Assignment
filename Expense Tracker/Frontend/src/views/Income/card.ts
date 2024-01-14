67c019f80349f689c36c415966d95fb00af9dcd8
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-14 15:27:06 +0545

    update add icons

--- Expense Tracker/Frontend/src/views/Income/card.ts
+++ Expense Tracker/Frontend/src/views/Income/card.ts
@@ -20,18 +20,18 @@ const createIncomeCard = (
   cardBody.classList.add("row", "m-0", "align-items-center", "col-12");
 
   const amount = document.createElement("div");
-  amount.classList.add("col-1", "text-success", "m-0", "align-middle");
+  amount.classList.add("col", "text-success", "m-0", "align-middle");
   amount.innerHTML = `+Rs. ${income.amount}`;
 
   const source = document.createElement("div");
   source.classList.add("col-6");
-  source.textContent = income.source;
+  source.innerHTML = `<i class="fa-solid fa-building"></i> ${income.source}`;
 
   const date = document.createElement("div");
   date.classList.add("col");
-  date.textContent = new Date(income.date as Date)
+  date.innerHTML = `<i class="fa-solid fa-calendar-days"></i> ${new Date(income.date as Date)
     .toUTCString()
-    .substring(5, 16);
+    .substring(5, 16)}`;
 
   const actions = document.createElement("div");
   actions.classList.add("col");
@@ -39,20 +39,26 @@ const createIncomeCard = (
   const editButton = document.createElement("button");
   editButton.innerHTML = "<i class='fa-solid fa-pen-to-square'></i>";
   editButton.classList.add("btn", "btn-outline-primary", "col");
-  editButton.addEventListener("click", editOnClick);
   editButton.setAttribute("data-bs-toggle", "tooltip");
   editButton.setAttribute("data-bs-placement", "top");
   editButton.setAttribute("data-bs-title", "Edit Income");
-  new bootstrap.Tooltip(editButton);
+  const editToolTip = new bootstrap.Tooltip(editButton);
+  editButton.addEventListener("click", () => {
+    editOnClick();
+    editToolTip.hide();
+  });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerHTML = "<i class='fa-solid fa-trash'></i>";
   deleteButton.classList.add("btn", "btn-outline-danger", "mx-2", "col");
-  deleteButton.addEventListener("click", deleteOnClick);
   deleteButton.setAttribute("data-bs-toggle", "tooltip");
   deleteButton.setAttribute("data-bs-placement", "top");
   deleteButton.setAttribute("data-bs-title", "Delete Income");
-  new bootstrap.Tooltip(deleteButton);
+  const deleteToolTip = new bootstrap.Tooltip(deleteButton);
+  deleteButton.addEventListener("click", () => {
+    deleteOnClick();
+    deleteToolTip.hide();
+  });
 
   cardBody.appendChild(amount);
   cardBody.appendChild(source);
