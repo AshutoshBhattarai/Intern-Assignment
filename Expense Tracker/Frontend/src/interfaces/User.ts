97684f52579472651b091c044c1f7783071846a4
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-14 16:03:47 +0545

    Update add user profile

--- Expense Tracker/Frontend/src/interfaces/User.ts
+++ Expense Tracker/Frontend/src/interfaces/User.ts
@@ -1,8 +1,8 @@
 interface User {
   id?: string;
   username?: string;
-  email: string;
-  password: string;
+  email?: string;
+  password?: string;
   createdAt?: Date;
   updatedAt?: Date;
 }
--- Expense Tracker/Frontend/src/views/Profile/script.ts
+++ Expense Tracker/Frontend/src/views/Profile/script.ts
@@ -9,6 +9,7 @@ import createPostRequest from "../../service/PostRequest";
 import createPutRequest from "../../service/PutRequest";
 import { showToast } from "../../components/Toast";
 import showErrorResponse from "../../service/ErrorResponse";
+import User from "../../interfaces/User";
 // --------------------- Getting elements from DOM -----------------------
 const navBar = document.getElementById("nav-placeholder") as HTMLElement;
 
@@ -16,9 +17,6 @@ const categoryContainer = document.getElementById(
   "category-container"
 ) as HTMLElement;
 
-const addCategoryDialog = document.getElementById(
-  "add-category-dialog"
-) as HTMLElement;
 const addCategoryBtn = document.getElementById(
   "btn-add-category"
 ) as HTMLElement;
@@ -36,25 +34,97 @@ const categoryTitleInput = document.getElementById(
 const categoryDescriptionInput = document.getElementById(
   "add-category-description"
 ) as HTMLInputElement;
+
+const displayUsername = document.getElementById(
+  "display-username"
+) as HTMLElement;
+const displayEmail = document.getElementById("display-email") as HTMLElement;
+const displayCreatedAt = document.getElementById(
+  "display-created-at"
+) as HTMLElement;
+const displayUpdatedAt = document.getElementById(
+  "display-updated-at"
+) as HTMLElement;
+const btnEditProfile = document.getElementById(
+  "btn-edit-profile"
+) as HTMLInputElement;
+
+const btnSaveProfile = document.getElementById(
+  "btn-save-profile"
+) as HTMLElement;
+const btnCloseProfileDialog = document.getElementById(
+  "btn-close-profile-dialog"
+) as HTMLElement;
+const btnUpdatePassword = document.getElementById(
+  "btn-change-password"
+) as HTMLElement;
+
+const btnSaveNewPassword = document.getElementById(
+  "btn-save-password"
+) as HTMLElement;
+const updateUsernameInput = document.getElementById(
+  "update-profile-username"
+) as HTMLInputElement;
+const updateEmailInput = document.getElementById(
+  "update-profile-email"
+) as HTMLInputElement;
+const updatePasswordInput = document.getElementById(
+  "update-user-password"
+) as HTMLInputElement;
+const updateConfirmPasswordInput = document.getElementById(
+  "update-user-repassword"
+) as HTMLInputElement;
+const btnClosePasswordDialog = document.getElementById(
+  "btn-close-password-dialog"
+) as HTMLElement;
 const toastContainer = document.getElementById("toast-message") as HTMLElement;
 // --------------------- Initializing Modals -----------------------
 
 let categoryModal: bootstrap.Modal;
 let categoryId: string = "";
+let updateProfileModal: bootstrap.Modal;
+let updatePasswordModal: bootstrap.Modal;
+/* -------------------------------------------------------------------------- */
+/*                            Initital onload tasks                           */
+/* -------------------------------------------------------------------------- */
 window.onload = async () => {
   renderNavBar(navBar, "nav-profile");
-  categoryModal = new bootstrap.Modal(addCategoryDialog);
-
+  categoryModal = new bootstrap.Modal(
+    document.getElementById("add-category-dialog") as HTMLElement
+  );
+  updateProfileModal = new bootstrap.Modal(
+    document.getElementById("update-user-dialog") as HTMLElement
+  );
+  updatePasswordModal = new bootstrap.Modal(
+    document.getElementById("update-password-dialog") as HTMLElement
+  );
   const userCategories = await getUserCategories();
   renderUserCategories(userCategories);
+  await renderUserDetails();
 };
 
+/* -------------------------------------------------------------------------- */
+/*                           Button Event Listeners                           */
+/* -------------------------------------------------------------------------- */
+btnEditProfile.addEventListener("click", () => {
+  updateUsernameInput.value = displayUsername.textContent!;
+  updateEmailInput.value = displayEmail.textContent!;
+  updateProfileModal.show();
+});
 addCategoryBtn.addEventListener("click", () => {
   categoryModal.show();
 });
 btnCloseCategoryDialog.addEventListener("click", () => {
-  closeDialog();
+  closeCategoryDialog();
 });
+
+btnCloseProfileDialog.addEventListener("click", () => {
+  closeUpdateProfileDialog();
+});
+btnClosePasswordDialog.addEventListener("click", () => {
+  closeUpdatePasswordDialog();
+});
+
 btnSaveCategory.addEventListener("click", async () => {
   const title = categoryTitleInput.value;
   const description = categoryDescriptionInput.value;
@@ -71,10 +141,10 @@ btnSaveCategory.addEventListener("click", async () => {
     categoryId = "";
   }
   renderUserCategories(await getUserCategories());
-  closeDialog();
+  closeCategoryDialog();
 });
 
-const closeDialog = () => {
+const closeCategoryDialog = () => {
   categoryTitleInput.value = "";
   categoryDescriptionInput.value = "";
   categoryModal.hide();
@@ -148,11 +218,11 @@ const saveCategory = async (category: Category) => {
     if (response.status == HttpStatusCode.Accepted) {
       renderUserCategories(await getUserCategories());
       showToast(response.data.message, toastContainer, "success");
-      closeDialog();
+      closeCategoryDialog();
     }
   } catch (error) {
     showErrorToast(error);
-    closeDialog();
+    closeCategoryDialog();
   }
 };
 
@@ -175,15 +245,102 @@ const updateCategory = async (category: Category) => {
     if (response.status == HttpStatusCode.Accepted) {
       renderUserCategories(await getUserCategories());
       showToast(response.data.message, toastContainer, "success");
-      closeDialog();
+      closeCategoryDialog();
     }
   } catch (error) {
-    closeDialog();
+    closeCategoryDialog();
     showErrorToast(error);
   }
 };
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const showErrorToast = (error: any) => {
-  const message = showErrorResponse(error) || error;
+  const message =
+    showErrorResponse(error) || error.response.data.message || error;
   showToast(message, toastContainer, "error");
+};
+const renderUserDetails = async () => {
+  const userDetails = await createGetRequest("/users/");
+  if (userDetails) {
+    const { username, email, updatedAt, createdAt } = userDetails.data;
+    displayUsername.textContent = username;
+    displayEmail.textContent = email;
+    displayUpdatedAt.textContent = new Date(updatedAt)
+      .toUTCString()
+      .substring(0, 16);
+    displayCreatedAt.textContent = new Date(createdAt)
+      .toUTCString()
+      .substring(0, 16);
+  }
+};
+const closeUpdateProfileDialog = () => {
+  updateProfileModal.hide();
+  updateUsernameInput.value = "";
+  updateEmailInput.value = "";
+};
+const closeUpdatePasswordDialog = () => {
+  updatePasswordModal.hide();
+  updatePasswordInput.value = "";
+  updateConfirmPasswordInput.value = "";
+};
+
+btnSaveProfile.addEventListener("click", async () => {
+  const newUsername = updateUsernameInput.value;
+  const newEmail = updateEmailInput.value;
+  const user: User = {};
+  user.email = newEmail;
+  user.username = newUsername;
+  await updateProfile(user);
+});
+btnUpdatePassword.addEventListener("click", () => {
+  closeUpdateProfileDialog();
+  updatePasswordModal.show();
+});
+
+btnSaveNewPassword.addEventListener("click", () => {
+  const newPassword = updatePasswordInput.value;
+  const newConfirmPassword = updateConfirmPasswordInput.value;
+  if (newPassword !== newConfirmPassword) {
+    showToast("Passwords do not match", toastContainer, "error");
+    return;
+  }
+  if (newPassword === "") {
+    showToast("Password cannot be empty", toastContainer, "error");
+    return;
+  }
+  const user: User = {};
+  user.password = newPassword;
+  updatePassword(user);
+});
+
+const updateProfile = async (user: User) => {
+  try {
+    if (user.username === "") {
+      showToast("Username cannot be empty", toastContainer, "error");
+      return;
+    }
+    if (user.email === "") {
+      showToast("Email cannot be empty", toastContainer, "error");
+      return;
+    }
+    const response = await createPutRequest("/users/", user);
+    if (response.status === HttpStatusCode.Accepted) {
+      renderUserDetails();
+      closeUpdateProfileDialog();
+      showToast(response.data.message, toastContainer, "success");
+    }
+  } catch (error) {
+    showErrorToast(error);
+  }
+};
+
+const updatePassword = async (user: User) => {
+  try {
+    const response = await createPutRequest("/users/", user);
+    if (response.status === HttpStatusCode.Accepted) {
+      showToast(response.data.message, toastContainer, "success");
+      closeUpdatePasswordDialog();
+    }
+  } catch (error) {
+    showErrorToast(error);
+  }
 };
