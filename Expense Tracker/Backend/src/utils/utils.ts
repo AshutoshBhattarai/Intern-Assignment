d6ca88cb254ee0f5d2d3a4c56293365d925c0647
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-14 17:25:48 +0545

    Update code clean up

--- Expense Tracker/Backend/src/utils/utils.ts
+++ Expense Tracker/Backend/src/utils/utils.ts
@@ -9,12 +9,10 @@ export const isSameDate = (date1: Date, date2: Date) => {
   const year2 = d2.getFullYear();
   const month2 = d2.getMonth();
   const day2 = d2.getDate();
-  if ((month1 == 0 && month2 == 0) ) {
-    if(day1 == 30 && day2 == 31) {
+  if (month1 == 0 && month2 == 0) {
+    if (day1 == 30 && day2 == 31) {
       day1 = 31;
     }
   }
-
-  console.log(year1, month1, day1, year2, month2, day2);
   return year1 === year2 && month1 === month2 && day1 === day2;
 };
