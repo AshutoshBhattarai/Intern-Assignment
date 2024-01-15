759a97db478fe5189afacdb1d84e3c371d40aa9b
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-01-15 21:24:08 +0545

    Update token check

--- Expense Tracker/Frontend/src/service/CheckAuth.ts
+++ Expense Tracker/Frontend/src/service/CheckAuth.ts
@@ -2,9 +2,10 @@ import TokenService from "./TokenService";
 
 const accessToken = TokenService.getAccessToken();
 const refreshToken = TokenService.getRefreshToken();
-
+// Check if token is available in local storage
+// If not, redirect to login
 const validateToken = async () => {
-  if (accessToken === "" && refreshToken === "") {
+  if (accessToken === null || refreshToken === null) {
     window.location.href = "/views/Login/";
   }
 };
